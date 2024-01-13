ac158fab72c28852a8522bc116c0ca649accd8a8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-01-13 03:19:54 +0000

    updates

--- lib/screens/MyProfileScreen/MyProfileScreen.dart
+++ lib/screens/MyProfileScreen/MyProfileScreen.dart
@@ -1,14 +1,28 @@
 import 'package:buddy/screens/SettingsScreen/SettingsScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class MyProfileScreen extends StatefulWidget {
-  const MyProfileScreen({super.key});
+  const MyProfileScreen({Key? key}) : super(key: key);
 
   @override
   State<MyProfileScreen> createState() => _MyProfileScreenState();
 }
 
 class _MyProfileScreenState extends State<MyProfileScreen> {
+  final List<String> imageUrls = [
+    "assets/images/boy.jpeg",
+    "assets/images/girl.jpeg",
+    "assets/images/p1.jpg",
+    "assets/images/p2.jpg",
+    "assets/images/p3.jpeg",
+    "assets/images/p4.jpeg",
+    "assets/images/p5.jpg",
+    "assets/images/p6.png",
+    "assets/images/p7.jpeg",
+    "assets/images/p8.jpeg"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +34,6 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              //profile text here
               const Padding(
                 padding: EdgeInsets.only(left: 8.0),
                 child: Text(
@@ -32,7 +45,6 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
                   ),
                 ),
               ),
-
               GestureDetector(
                 onTap: () {
                   Navigator.of(context).push(MaterialPageRoute(
@@ -56,172 +68,183 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  boxShadow: [
-                    BoxShadow(
-                      color: const Color(0xFF17203A).withOpacity(0.3),
-                      offset: const Offset(2, 5),
-                      blurRadius: 10.0,
-                    ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 30.0),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        //profile image here
-                        Container(
-                          decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: const Color(0xFF17203A).withOpacity(0.3),
-                                offset: const Offset(2, 5),
-                                blurRadius: 20.0,
-                              ),
-                            ],
-                          ),
-                          child: const CircleAvatar(
-                            radius: 40.0,
-                            backgroundImage:
-                                AssetImage('assets/images/boy.jpeg'),
-                          ),
-                        ),
-
-                        //name here
-                        const Padding(
-                          padding: EdgeInsets.only(top: 20.0),
-                          child: Text(
-                            'John Doe',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
+      body: Column(
+        children: [
+          // Profile information
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0xFF17203A).withOpacity(0.3),
+                    offset: const Offset(2, 5),
+                    blurRadius: 10.0,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //profile image here
+                      Container(
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: const Color(0xFF17203A).withOpacity(0.3),
+                              offset: const Offset(2, 5),
+                              blurRadius: 20.0,
                             ),
+                          ],
+                        ),
+                        child: const CircleAvatar(
+                          radius: 40.0,
+                          backgroundImage: AssetImage('assets/images/boy.jpeg'),
+                        ),
+                      ),
+
+                      //name here
+                      const Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: Text(
+                          'John Doe',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
                           ),
                         ),
+                      ),
 
-                        //bio here
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10.0),
-                          child: Text(
-                            'Lorem Ipsum is simply dummy hhh',
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: Colors.grey[600],
+                      //bio here
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          'Lorem Ipsum is simply dummy hhh',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ),
+
+                      //location here
+                      Padding(
+                        padding: const EdgeInsets.only(top: 3.0),
+                        child: Text(
+                          'Accra, Ghana',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ),
+
+                      //number of post, buddies and buddying
+                      Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            //number of posts
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Posts',
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 2.0),
+                                  child: Text(
+                                    '517',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
-                        ),
 
-                        //location here
-                        Padding(
-                          padding: const EdgeInsets.only(top: 3.0),
-                          child: Text(
-                            'Accra, Ghana',
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: Colors.grey[600],
+                            //number of buddies
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Buddies',
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 2.0),
+                                  child: Text(
+                                    '602',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
+
+                            //number of buddying
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Buddying',
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 2.0),
+                                  child: Text(
+                                    '376',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-
-                        //number of post, buddies and buddying
-                        Padding(
-                          padding: const EdgeInsets.all(30.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              //number of posts
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Posts',
-                                    style: TextStyle(
-                                      fontSize: 14.0,
-                                      color: Colors.grey[600],
-                                    ),
-                                  ),
-                                  const Padding(
-                                    padding: EdgeInsets.only(top: 2.0),
-                                    child: Text(
-                                      '517',
-                                      style: TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-
-                              //number of buddies
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Buddies',
-                                    style: TextStyle(
-                                      fontSize: 14.0,
-                                      color: Colors.grey[600],
-                                    ),
-                                  ),
-                                  const Padding(
-                                    padding: EdgeInsets.only(top: 2.0),
-                                    child: Text(
-                                      '602',
-                                      style: TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-
-                              //number of buddying
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Buddying',
-                                    style: TextStyle(
-                                      fontSize: 14.0,
-                                      color: Colors.grey[600],
-                                    ),
-                                  ),
-                                  const Padding(
-                                    padding: EdgeInsets.only(top: 2.0),
-                                    child: Text(
-                                      '376',
-                                      style: TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-
-              //media here
-            ],
+            ),
           ),
-        ),
+
+          // Media section
+          Expanded(
+            child: MasonryGridView.builder(
+              itemCount: 10,
+              gridDelegate:
+                  const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: Image.asset(imageUrls[index]),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
